f0e7f9c75a64e9a9fc66f675735f883cb115d505
Marian Ban <marian.ban@gmail.com>
2025-10-11 15:18:18 +0200

    core

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
+    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+  parser: tsParser,
+      parserOptions: {
+        project: false,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    },
+  },
+];
