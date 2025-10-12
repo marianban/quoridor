b850a2185fd1e37d244898b8fd884c62973c58af
Marian Ban <marian.ban@gmail.com>
2025-10-12 09:31:12 +0200

    react eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,32 @@
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import reactPlugin from 'eslint-plugin-react';
 
-export default defineConfig(tseslint.configs.recommended, {
-  files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
-  ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**'],
-  languageOptions: {
-    globals: {
-      ...globals.node,
+export default defineConfig(
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
+    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     },
   },
-  rules: {
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+  {
+    basePath: 'packages/web-html',
+    ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**'],
+    ...reactPlugin.configs.flat.recommended,
+    ...reactPlugin.configs.flat['jsx-runtime'],
+    languageOptions: {
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+    },
   },
-});
+);
